4497f6c63368e09a1491aaec4503d303ebce275c
Zakyboss <boochimo8@gmail.com>
2024-09-10 22:54:49 +0300

    correction

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 print(''.join(
-    '{}'.format(chr(i)) 
-    for i in range(97, 123) 
+    '{}'.format(chr(i))
+    for i in range(97, 123)
     if chr(i) not in 'qe'
 ), end='')
